dcded7257ec7c1ee6e706d11a420abb7a3829d10
Muskan044 <muskankhan430430@gmail.com>
2024-07-10 12:43:27 +0500

    add tasks

---
+++ script.js
@@ -0,0 +1,45 @@
+// smallest number in array
+// var numbers = [5, 3, 7, 10]
+// var smallest = numbers[0]
+
+// for (var i = 0; i < numbers.length; i++){
+//     if(smallest > numbers[i]){
+//       smallest = numbers[i]
+//     }
+// }
+// console.log(smallest)
+
+// // 02
+
+// var calcius = +prompt("enter temperature in calcius")
+// var fahrenheit = (calcius * 9 / 5) + 32;
+// console.log(`${calcius}C is equal to ${fahrenheit}F`)
+
+// 03
+// var fahrenheit = +prompt("enter temperature in fahrenheit")
+// var calcius = (fahrenheit - 32) * 5 / 9;
+// console.log(`${fahrenheit}F is equal to ${calcius}C`)
+
+// 04
+// var numbersArray = [1, 2, 3, 4, 5];
+// console.log("original array: ");
+// console.log(numbersArray);
+// numbersArray.reverse();
+// console.log("Reversed Array: ");
+// console.log(numbersArray);
+
+// 05
+// var numbers = [];
+// for (var i = 0; i < 5; i++){
+//     var num = +prompt("enter a number: ")
+//     numbers.push(num)
+// }
+// console.log(numbers)
+
+// var sum = 0
+// var avg = 0
+// for (var i = 0; i < numbers.length; i++){
+//     sum += numbers[i];
+//     avg = sum / numbers.length;
+// }
+// console.log(avg)
